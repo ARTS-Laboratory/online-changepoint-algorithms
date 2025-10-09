08c6f73ca067b7abb3fcaa7ccc7eb6eaa1c6afb2
zhymir <dzhymir@gmail.com>
2025-10-09 00:19:11 -0400

    Added tests.

--- rust/src/expect_max/em_model.rs
+++ rust/src/expect_max/em_model.rs
@@ -172,3 +172,68 @@ impl EmModel {
         densities / (size as f64)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::expect_max::em_model_builder::EmBuilderOne;
+    use super::*;
+    
+    #[test]
+    fn test_build_model() {
+        let abnormals = vec![NormalParams::from_tuple((30.0, 1.0, 0.25)).unwrap()];
+        let samples = vec![-1.0, 0.0, 1.0, 30.0, 29.0, 31.0];
+        let mut model_one = EmBuilderOne::new();
+        let em_model = model_one
+            .build_normal(0.0, 1.0, 0.5).unwrap()
+            .build_abnormal(&abnormals)
+            .build_samples_from_slice(&samples)
+            .next_builder().unwrap()
+            .build_likelihoods()
+            .next_builder().unwrap()
+            .get_standard_model();
+        assert_eq!(em_model.epochs, PositiveInteger::new(1).unwrap());
+    }
+    
+    fn make_standard_model() -> EmModel {
+        let abnormals = vec![NormalParams::from_tuple((30.0, 1.0, 0.25)).unwrap()];
+        let samples = vec![-1.0, 0.0, 1.0, 30.0, 29.0, 31.0];
+        let mut model_one = EmBuilderOne::new();
+        model_one
+            .build_normal(0.0, 1.0, 0.5).unwrap()
+            .build_abnormal(&abnormals)
+            .build_samples_from_slice(&samples)
+            .next_builder().unwrap()
+            .build_likelihoods()
+            .next_builder().unwrap()
+            .get_standard_model()
+    }
+
+    #[test]
+    fn test_swap_last_sample() {
+        let mut model = make_standard_model();
+        let last = model.swap_last_sample(42.0);
+        assert_eq!(last, 0.0);
+        assert_eq!(model.samples.last(), Some(&42.0));
+    }
+
+    #[test]
+    #[should_panic( expected = "samples is empty")]
+    fn test_swap_last_sample_empty() {
+        let normal = NormalParams::from_tuple((0.0, 1.0, 0.5)).unwrap();
+        let abnormals = vec![NormalParams::from_tuple((30.0, 1.0, 0.25)).unwrap()];
+        let samples = Array1::<f64>::zeros(0);
+        let epochs = PositiveInteger::new(2).unwrap();
+        let mut model = EmModel::new(normal, abnormals, samples, epochs);
+        let res = model.swap_last_sample(-14.0);
+    }
+
+    #[test]
+    fn test_update() {
+        let mut model = make_standard_model();
+        let old_likelihoods = model.likelihoods.clone();
+        model.update(0.0).unwrap();
+        assert_ne!(old_likelihoods, model.likelihoods);
+
+    }
+
+}
