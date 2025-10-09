a393c1b05768299e9adc86fa2d19352a943c0eff
zhymir <dzhymir@gmail.com>
2025-10-09 17:35:37 -0400

    Added more tests and comments to separate them.

--- rust/src/expect_max/em_model_builder.rs
+++ rust/src/expect_max/em_model_builder.rs
@@ -277,6 +277,8 @@ impl EmBuilderLast<f64> {
 mod tests {
     use super::*;
 
+    // EmBuilderOne tests
+
     #[test]
     fn test_em_builder_one_build_normal() {
         let mut em = EmBuilderOne::new();
@@ -303,5 +305,140 @@ mod tests {
         let mut em = EmBuilderOne::new();
         let result = em.build_normal(0.0, 2.0, 1.5);
         assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_em_builder_one_build_abnormal() {
+        let mut em = EmBuilderOne::new();
+        assert!(em.abnormals.is_empty());
+        let values = vec![
+            NormalParams::new(Normal::new(0.0, 1.0).unwrap(), 0.5).unwrap(),
+            NormalParams::new(Normal::new(1.0, 2.0).unwrap(), 0.5).unwrap()];
+        em.build_abnormal(&values);
+        assert_eq!(em.abnormals.get(0), Some(&values[0]));
+        assert_eq!(em.abnormals.get(1), Some(&values[1]));
+    }
+
+    #[test]
+    fn test_em_builder_one_build_abnormal_from_tuples() {
+        let mut em = EmBuilderOne::new();
+        assert!(em.abnormals.is_empty());
+        let values = vec![
+            (0.0, 1.0, 0.5),
+            (1.0, 2.0, 0.5)];
+        let result = em.build_abnormal_from_tuples(&values);
+        assert!(result.is_ok());
+        assert_eq!(em.abnormals.get(0), Some(&NormalParams::from_tuple(values[0]).unwrap()));
+        assert_eq!(em.abnormals.get(1), Some(&NormalParams::from_tuple(values[1]).unwrap()));
+    }
+
+    #[test]
+    fn test_em_builder_one_build_epochs() {
+        let mut em = EmBuilderOne::new();
+        let result = em.build_epochs(10);
+        assert!(result.is_ok());
+        assert_eq!(em.epochs, PositiveInteger::new(10).unwrap());
+        // Failing case
+        let result = em.build_epochs(0);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_build_samples_from_slice() {
+        let mut em = EmBuilderOne::new();
+        let samples = vec![0.0, 2.0, -1.0];
+        em.build_samples_from_slice(&samples);
+        let Complete(new_samples) = em.sample_arr else { panic!("Sample array not initialized") };
+        assert_eq!(new_samples, Array1::from_vec(vec![0.0, 2.0, -1.0, 0.0]));
+    }
+
+    #[test]
+    fn test_em_builder_one_next_builder() {
+        let mut em = EmBuilderOne::new();
+        let result = em.next_builder();
+        assert!(result.is_err());
+        let samples = vec![0.0, 2.0, -1.0];
+        em.build_samples_from_slice(&samples);
+        let result = em.next_builder();
+        assert!(result.is_ok());
+        let new_samples = result.unwrap().sample_arr;
+        assert_eq!(new_samples, Array1::from_vec(vec![0.0, 2.0, -1.0, 0.0]));
+    }
+
+    // EmBuilderTwo tests
+
+    fn make_em_builder_two() -> EmBuilderTwo<f64> {
+        let mut em = EmBuilderOne::new();
+        let samples = vec![0.0, 2.0, -1.0];
+        em.build_samples_from_slice(&samples);
+        let result = em.next_builder();
+        assert!(result.is_ok());
+        result.unwrap()
+    }
+
+    #[test]
+    fn test_em_builder_two_build_likelihoods() {
+        let mut em = make_em_builder_two();
+        em.build_likelihoods();
+        let Complete(new_likelihoods) = em.likelihoods_arr else { panic!("Likelihoods array not initialized") };
+        assert_eq!(new_likelihoods.shape(), &[1, 4]);
+    }
+
+    #[test]
+    fn test_em_builder_two_next_builder() {
+        let mut em = make_em_builder_two();
+        let result = em.next_builder();
+        assert!(result.is_err());
+        if let Err(e) = result {
+            println!("{:?}", e);
+        }
+        em.build_likelihoods();
+        let result = em.next_builder();
+        assert!(result.is_ok());
+        let new_likelihoods = result.unwrap().likelihoods_arr;
+        assert_eq!(new_likelihoods.shape(), &[1, 4]);
+    }
+
+    // EmBuilderLast tests
+
+    fn make_em_builder_last() -> EmBuilderLast<f64> {
+        let mut em = EmBuilderOne::new();
+        let samples = vec![0.0, 2.0, -1.0];
+        em.build_samples_from_slice(&samples);
+        let result = em.next_builder();
+        assert!(result.is_ok());
+        result.unwrap().build_likelihoods().next_builder().unwrap()
+    }
+
+    #[test]
+    fn test_get_standard_model() {
+        let mut em = make_em_builder_last();
+        let standard_model = em.get_standard_model();
+        assert_eq!(standard_model.normal, em.normal);
+        assert_eq!(standard_model.abnormals, em.abnormals);
+        assert_eq!(standard_model.samples, em.sample_arr);
+        assert_eq!(standard_model.likelihoods, em.likelihoods_arr);
+        assert_eq!(standard_model.epochs, em.epochs);
+    }
+
+    #[test]
+    fn test_get_early_stop_model() {
+        let mut em = make_em_builder_last();
+        em.build_likelihood_converge_checker();
+        let early_stop_model = em.get_early_stop_model();
+        let EarlyStopEmModel { em_model, converge_checker } = early_stop_model;
+        assert_eq!(em_model.normal, em.normal);
+        assert_eq!(em_model.abnormals, em.abnormals);
+        assert_eq!(em_model.samples, em.sample_arr);
+        assert_eq!(em_model.likelihoods, em.likelihoods_arr);
+        assert_eq!(em_model.epochs, em.epochs);
+        assert_eq!(converge_checker.prev_likelihood.shape(), em.likelihoods_arr.shape());
+    }
+
+    #[test]
+    #[should_panic(expected = "Converge checker not initialized")]
+    fn test_get_early_stop_model_panics_without_converge_checker() {
+        let mut em = make_em_builder_last();
+        em.get_early_stop_model();
     }
 }
